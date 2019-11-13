d3f46b08ca8171eca04ae6682c9889172aff7383
Codewebs <devs.indiza@gmail.com>
2019-11-13 20:29:40 +0100

    commitable

--- admin/config.php
+++ admin/config.php
@@ -25,12 +25,11 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'tchop237');
+define('DB_HOSTNAME', 'us-cdbr-iron-east-05.cleardb.net');
+define('DB_USERNAME', 'bdc544f82295b8');
+define('DB_PASSWORD', '65169927');
+define('DB_DATABASE', 'heroku_d5243f28b216730');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
-
 // OpenCart API
 define('OPENCART_SERVER', 'https://www.opencart.com/');
