b17bc4bf3f8bc84857f0d3876a5f018855199850
Codewebs <devs.indiza@gmail.com>
2019-11-15 12:25:16 +0100

    changes of configuation of server 5

--- admin/config.php
+++ admin/config.php
@@ -12,7 +12,7 @@ define('DIR_APPLICATION', './');
 define('DIR_SYSTEM', '../system/');
 define('DIR_IMAGE', '../image/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_CATALOG', 'C:/xampp/htdocs/tchop237/catalog/');
+define('DIR_CATALOG', '../catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
